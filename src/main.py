c5c43835d8f65d13da3506191c2009b13ca7ae58
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-05 16:30:24 -0700

    Added logic for program

--- src/main.py
+++ src/main.py
@@ -4,5 +4,28 @@ Created on Aug 31, 2017
 @author: jlearx
 '''
 
+def loadBdays():
+    birthdays = {}
+    return birthdays
+
+def listBdays(birthdays):
+    for k in birthdays.keys():
+        print(k)
+    
+def getBday(birthdays, name):
+    if (name in birthdays):
+        print("{}'s birthday is {}.".format(name, birthdays[name]))
+    else:
+        print("Sorry, I don't know {}'s birthday.".format(name))
+
 if __name__ == '__main__':
-    pass
+    birthdays = loadBdays()
+    
+    print("Welcome to the birthday dictionary. We know the birthdays of:")
+    listBdays(birthdays)
+    
+    name = input("Whose birthday do you want to look up? ")
+    name = name.strip()
+    
+    getBday(birthdays, name)
+    
