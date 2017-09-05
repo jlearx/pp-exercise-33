a118bb3c3370c6ecadf52cca25642e21f896bb06
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-05 16:34:33 -0700

    Added people to the dictionary.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,13 @@ Created on Aug 31, 2017
 '''
 
 def loadBdays():
-    birthdays = {}
+    birthdays = {
+        'Albert Einstein': '03/14/1879',
+        'Benjamin Franklin': '01/17/1706',
+        'Ada Lovelace': '12/10/1815',
+        'Donald Trump': '06/14/1946',
+        'Rowan Atkinson': '01/6/1955'}
+    
     return birthdays
 
 def listBdays(birthdays):
